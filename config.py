d4c735c1436cfc48b5d5161bfd14ac82e662029c
Sean Noyes <sean.noyes@gmail.com>
2025-03-18 11:59:36 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+redis_host = os.environ.get("REDIS_HOST")
+redis_password = os.environ.get("REDIS_PASSWORD")
+redis_port = os.environ.get("REDIS_PORT")
+redis_user = os.environ.get("REDIS_USER")
